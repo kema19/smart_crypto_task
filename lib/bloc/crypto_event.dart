1e035485dd4090c40c68578793210ceb2071b845
kema19 <kema19101985@gmail.com>
2026-03-20 11:34:26 +0500

    Update crypto_event.dart

--- lib/bloc/crypto_event.dart
+++ lib/bloc/crypto_event.dart
@@ -5,7 +5,9 @@ class FetchCryptoData extends CryptoEvent {}
 class FilterGainers extends CryptoEvent {}
 
 class ResetFilters extends CryptoEvent {}
+class FilterFallEvent extends CryptoEvent {}
 
+class FilterTop10Event extends CryptoEvent {}
 class ToggleFavorite extends CryptoEvent {
   final String id;
   ToggleFavorite(this.id);
